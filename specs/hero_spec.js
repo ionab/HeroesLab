4f3b01f48ecd4a3e66a4792f48287219317b984c
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-07 16:20:49 +0100

    tests drafted

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var Hero = require('../hero.js');
 var Tasks = require('../task.js');
-
+var Food = require('../food.js')
 
 describe('Hero', function(){
   var hero1;
@@ -29,5 +29,42 @@ describe('Hero', function(){
   it("it should have an array of tasks which start empty", function(){
     assert.strictEqual(hero1.tasks.length, 0);
   })
+
+  it("should be able to eat food, and health value should go up by the replenishmentValue", function(){
+    food = new Food("testFood", 5)
+    hero1.eatFood(food)
+    assert.strictEqual(hero.health, 15)
+  })
+
+  it("should increase health by 1.5 * value if the food is their favourite food", function(){
+
+  })
+
+  it("should be able to sort task by difficulty", function(){
+
+  })
+
+  it("should be able to sort task by urgency", function(){
+
+  })
+
+  it("should be able to sort task by reward", function(){
+
+  })
+
+  it("should be able to view tasks that are marked as complete", function(){
+
+
+  })
+
+  it("should be able to view tasks that are marked as incomplete", function(){
+
+
+  })
+
+
+
+
+
 
 })
